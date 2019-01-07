0ef4b847f415dd43c8c051b47e3be5449ee33711
Gilbert Torchon <gilbert@noukod.com>
2019-01-07 17:52:35 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,16 +3,16 @@ class Dog
   attr_accessor :name,:age,:breed
 
   @@all=[]
-  
+
   def initialize(name=nil,breed=nil,age=nil)
     @name=name
     @breed=breed
     @age=age
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
+  def self.all
     @@all
   end
-  
+
 end
